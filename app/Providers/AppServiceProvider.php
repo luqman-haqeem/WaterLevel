65767ce5370be239aa9e403c6c7dfe7a827e0825
Luqman <62147517+luqman1412@users.noreply.github.com>
2023-01-29 21:22:17 +0800

    force  HTTPS when env  prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if(config('app.env') === 'production') {
+        if(env('APP_ENV') === 'production') {
             \URL::forceScheme('https');
         }
         Paginator::useBootstrap();
