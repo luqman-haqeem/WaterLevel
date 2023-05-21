8ac122873358ca5261a86eaabeb9094161ab4c2d
Luqman <62147517+luqman1412@users.noreply.github.com>
2023-05-21 12:41:13 +0800

    use update batch in cron

--- app/Http/Controllers/API/CronController.php
+++ app/Http/Controllers/API/CronController.php
@@ -10,55 +10,108 @@ use Illuminate\Http\Request;
 
 class CronController extends Controller
 {
-    //
     /**
-     * Update The  current water level.
+     * Update the current water level.
      *
-     * @param  \App\Models\Camera  $camera
      * @return \Illuminate\Http\Response
      */
     public function update()
     {
-        //
         ignore_user_abort(true);
 
         $districts = Districts::all();
 
-        foreach ($districts as $district) {
-            $curl = curl_init();
+        // Prepare the bulk update data array
+        $bulkUpdateData = [];
 
+        foreach ($districts as $district) {
+            $curls = [];
+
+            $curlMultiHandler = curl_multi_init();
+
+            $url =  'http://infobanjirjps.selangor.gov.my/JPSAPI/api/StationRiverLevels/GetWLAllStationData/' . $district->id;
+            $curl = curl_init();
             curl_setopt_array($curl, array(
-                CURLOPT_URL => 'http://infobanjirjps.selangor.gov.my/JPSAPI/api/StationRiverLevels/GetWLAllStationData/' . $district->id,
+                CURLOPT_URL => $url,
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_ENCODING => '',
                 CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
+                CURLOPT_TIMEOUT => 5,
                 CURLOPT_FOLLOWLOCATION => true,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => 'GET',
             ));
+            curl_multi_add_handle($curlMultiHandler, $curl);
+            $curls[] = $curl;
 
-            $response = curl_exec($curl);
-            if (curl_errno($curl)) {
-                echo "Echo: " . curl_error($curl);
-            }
-            curl_close($curl);
-            $stationsJps  = json_decode($response);
+            // Execute asynchronous requests
+            $active = null;
+            do {
+                $status = curl_multi_exec($curlMultiHandler, $active);
+            } while ($status === CURLM_CALL_MULTI_PERFORM || $active);
 
-            foreach ($stationsJps->stations as $stationJps) {
-                $station = Station::where('JPS_sel_id', $stationJps->id)->first();
-                if (empty($station)) {
-                } else {
-                    $currentLevel = CurrentLevel::where('station_id', $station->id)->first();
-                    $currentLevel->update(
-                        [
-                            'current_level' => $stationJps->waterLevel,
-                            'alert_level' => $stationJps->waterlevelStatus
-                        ]
+            // Process responses
+            foreach ($curls as $curl) {
+                $response = curl_multi_getcontent($curl);
+
+                if (curl_errno($curl)) {
+                    echo "Error: " . curl_error($curl);
+                }
+
+                $stationsJps = json_decode($response);
+
+                if (!isset($stationsJps->stations)) {
+                    return response()->json(
+                        ['error' => 'Cannot fetch data from JPS selangor', "url" => $url],
+                        500
                     );
                 }
+                // print_r($stationsJps->stations);
+                foreach ($stationsJps->stations as $stationJps) {
+                    $station = Station::where('JPS_sel_id', $stationJps->id)->first();
+
+                    if ($station) {
+                        $bulkUpdateData[] = [
+                            'station_id' => $station->id,
+                            'current_level' => $stationJps->waterLevel,
+                            'alert_level' => $stationJps->waterlevelStatus,
+                        ];
+                    }
+                }
+
+                curl_multi_remove_handle($curlMultiHandler, $curl);
             }
+
+            curl_multi_close($curlMultiHandler);
         }
-        return response()->noContent();
+        // dd($bulkUpdateData);
+        // Perform bulk update
+        if (!empty($bulkUpdateData)) {
+            $currentLevelModel = new CurrentLevel();
+
+            $table = $currentLevelModel->getTable();
+
+            $cases = [];
+            $ids = [];
+            $updatedDate = date('Y-m-d H:i:s');
+
+            foreach ($bulkUpdateData as $data) {
+                $stationId = $data['station_id'];
+                $currentLevel = $data['current_level'];
+                $alertLevel = $data['alert_level'];
+
+                $cases[] = "WHEN {$stationId} THEN {$currentLevel}";
+                $ids[] = $stationId;
+            }
+
+            $ids = implode(',', $ids);
+            $cases = implode(' ', $cases);
+
+            $query = "UPDATE {$table} SET current_level = (CASE id {$cases} END), alert_level = (CASE id {$cases} END), updated_at = '{$updatedDate}' WHERE id IN ({$ids})";
+
+            \DB::update(\DB::raw($query));
+
+            return response()->noContent();
+        }
     }
 }
